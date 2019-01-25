87f74815cf71a018f92d6dea819e09fadec12bdc
Kol4 <fokler18@gmail.com>
2019-01-25 12:49:13 +0300

    mb the latest version :D

--- src/main/java/com/epam/kdubka/main/Application.java
+++ src/main/java/com/epam/kdubka/main/Application.java
@@ -3,18 +3,20 @@ package com.epam.kdubka.main;
 
 import com.epam.kdubka.service.ActionService;
 import com.epam.kdubka.service.ArrayListCreator;
+import com.epam.kdubka.service.MergeSort;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 class Application {
     public static void main(String[] args) {
-        String filePath = "/test.txt";
+        String filePath = "/files/test.txt";
         ArrayListCreator creator = new ArrayListCreator();
         ArrayList<Float> numbers = creator.readFile(filePath);
         System.out.println("Created list is " + numbers);
         ActionService action = new ActionService();
-        System.out.println("Sorted list is " + Arrays.toString(action.sort(numbers)));
+        MergeSort sorter = new MergeSort();
+        System.out.println("Sorted list is " + Arrays.toString((sorter.sort(numbers))));
         float minValue = action.getMin(numbers);
         float maxValue = action.getMax(numbers);
         float middleValue = action.getMiddle(numbers);
--- src/main/java/com/epam/kdubka/service/ActionService.java
+++ src/main/java/com/epam/kdubka/service/ActionService.java
@@ -4,45 +4,6 @@ import java.util.ArrayList;
 
 
 public class ActionService {
-    public float[] sort(ArrayList<Float> list) {
-        float[] floats = new float[list.size()];
-        int n = floats.length;
-        for (int i = 0; i < list.size(); i++) {
-            floats[i] = list.get(i);
-        }
-        mergeSort(floats, 0, n - 1);
-        return floats;
-    }
-
-    public static void mergeSort(float[] numbers, int low, int high) {
-        if (low < high) {
-            int mid = (low + high) / 2;
-            mergeSort(numbers, low, mid);
-            mergeSort(numbers, mid + 1, high);
-            merge(numbers, low, mid, high);
-        }
-    }
-
-    private static void merge(float[] buf, int low, int mid, int high) {
-        int n = high - low + 1;
-        float[] Temp = new float[n];
-        int l = low, j = mid + 1;
-        int k = 0;
-        while (l <= mid || j <= high) {
-            if (l > mid)
-                Temp[k++] = buf[j++];
-            else if (j > high)
-                Temp[k++] = buf[l++];
-            else if (buf[l] < buf[j])
-                Temp[k++] = buf[l++];
-            else
-                Temp[k++] = buf[j++];
-        }
-        for (j = 0; j < n; j++)
-            buf[low + j] = Temp[j];
-    }
-
-
     public float getMin(ArrayList<Float> list) {
         try {
             float min = list.get(0);
